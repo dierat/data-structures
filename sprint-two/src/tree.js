15189f25cdd37501a0894beff6058b4b34b7e774
dierat <diedra.rater@gmail.com>
2015-07-03 12:10:58 -0700

    adds working tree code

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,8 @@ var Tree = function(value){
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  _.extend(newTree, treeMethods);
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -15,11 +16,39 @@ var Tree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+	
+	this.children.push( Tree(value) );
 
 };
 
 treeMethods.contains = function(target){
 
+	var check = false;
+
+	var search = function(target, node){
+
+		_.each(node.children, function(child) {
+			if ( child.value === target ){
+				check = true;
+			} else{
+				search(target, child);
+			}
+		});
+
+		/*if( node.children.length > 0){
+			if ( node.children[0].value === target ){
+				check = true;
+			} 
+		} else if (node.children[0].children.length > 0 ){
+			_.each(node.children[0].children, function(child){
+				return search(target, child);
+			});
+		}*/
+	}
+
+	search(target, this);
+	
+	return check;
 };
 
 
