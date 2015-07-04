e3c8228b607b3abf840d47bb7f5bb55e32929905
dierat <diedra.rater@gmail.com>
2015-07-03 19:13:05 -0700

    changes test for checking if edge was removed to actually test if edge was removed

--- sprint-two/spec/graphSpec.js
+++ sprint-two/spec/graphSpec.js
@@ -39,8 +39,10 @@ describe('graph', function() {
   it('should remove edges between nodes', function() {
     graph.addNode('apples');
     graph.addNode('satsumas');
-    graph.addEdge('satsumas', 'apples');
+    graph.addEdge('apples', 'satsumas');
     expect(graph.hasEdge('apples', 'satsumas')).to.equal(true);
+    graph.removeEdge('apples', 'satsumas');
+    expect(graph.hasEdge('apples', 'satsumas')).to.equal(false);
   });
 
   it('should execute a callback on each node in the graph', function() {
