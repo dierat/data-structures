ff558a6c0b93a8ef1664c2539c066be237a07eee
dierat <diedra.rater@gmail.com>
2015-07-04 15:49:05 -0700

    adds method definitions and working insert code

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,42 @@
 var BinarySearchTree = function(value){
+  var tree = {
+  	value: value,
+  	left: undefined,
+  	right: undefined
+  };
+
+  tree.insert = function(value){
+    var newTree = BinarySearchTree(value);
+
+    var searchAndInsert = function(newTree, currenTree){
+      // see if newTree's value is less than currenTree's value
+      if (newTree.value < currenTree.value){
+        if (currenTree.left === undefined){
+    	  currenTree.left = newTree;
+        } else {
+          searchAndInsert(newTree, currenTree.left);
+        }
+      } else {
+        if (currenTree.right === undefined){
+          currenTree.right = newTree;
+        } else {
+          searchAndInsert(newTree, currenTree.right);
+        }
+      }
+    }
+
+    searchAndInsert(newTree, tree);
+  }
+
+  tree.contains = function(value){
+
+  }
+
+  tree.depthFirstLog = function(){
+
+  }
+
+  return tree;
 };
 
 
