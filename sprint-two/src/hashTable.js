7db43828a173ca4c89b9e754c9e15d0f106c7654
dierat <diedra.rater@gmail.com>
2015-07-04 19:28:48 -0700

    adds new data structure example in comment, also exchanges tabs for double-spaces and edits an outdated comment

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,22 +5,17 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var bucketIndex = getIndexBelowMaxForKey(k, this._limit);
-
-	// call this.retrieve() to retrieve existing bucket
-	var retrieve = this._storage.get(bucketIndex);
-
-	// check result for undefined
-	if ( retrieve === undefined ){
-	
-	// if undefined set result as key: value pair within object
-		retrieve = {};
-	}
-
-	// if defined add key: value pair to exisiting object
-	retrieve[k] = v;
-
-	// call this.set() to place inside storage at bucketIndex
-	this._storage.set(bucketIndex, retrieve);
+  // call this.retrieve() to retrieve existing bucket
+  var retrieve = this._storage.get(bucketIndex);
+  // check result for undefined
+  if ( retrieve === undefined ){
+    // if undefined set result as object
+    retrieve = {};
+  }
+  // if defined add key: value pair to exisiting object
+  retrieve[k] = v;
+  // call this.set() to place inside storage at bucketIndex
+  this._storage.set(bucketIndex, retrieve);
 };
 
 HashTable.prototype.retrieve = function(k){
@@ -39,20 +34,20 @@ HashTable.prototype.retrieve = function(k){
 };
 
 HashTable.prototype.remove = function(k){
-
-	 var bucketIndex = getIndexBelowMaxForKey(k, this._limit);
-
-	 // retrieve bucket
-	 var retrieve = this._storage.get(bucketIndex);
-
-	 // setting obj key: value to null
-	 retrieve[k] = null;
-
-	 // set back into storage
-	 this._storage.set(bucketIndex, retrieve);
+  var bucketIndex = getIndexBelowMaxForKey(k, this._limit);
+   // retrieve bucket
+   var retrieve = this._storage.get(bucketIndex);
+   // setting obj key: value to null
+   retrieve[k] = null;
+   // set back into storage
+   this._storage.set(bucketIndex, retrieve);
 };
 
+ /*
 
+this._storage = [  [ [],[] ], [ ['key1','val1'],['key2','val2'] ]  ]
+
+ */
 
 /*
  * Complexity: What is the time complexity of the above functions?
